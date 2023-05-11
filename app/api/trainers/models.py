d9b0720e64efb048fc3c585e027332ea560f3679
Juan <jaguirreb@fi.uba.ar>
2023-05-11 18:49:39 -0300

    Now an user can get the training plans associated with an specific user

--- app/api/trainers/models.py
+++ app/api/trainers/models.py
@@ -1,7 +1,6 @@
 from enum import Enum
 from pydantic import BaseModel, Field, PyObject
-from uuid import uuid4, UUID
-
+from uuid import uuid4
 
 # class PyObjectId(ObjectId):
 #     @classmethod
@@ -17,7 +16,7 @@ from uuid import uuid4, UUID
 #     @classmethod
 #     def __modify_schema__(cls, field_schema):
 #         field_schema.update(type="string")
-        
+
 
 MAX_TITLE_LENGTH = 200
 MIN_TITLE_LENGTH = 3
@@ -29,37 +28,69 @@ class Difficulty(str, Enum):
     intermediate = "intermediate"
     advanced = "advanced"
 
-    
+
+REVIEW_MAX_LENGTH = 240
+
+
+class Review(BaseModel):
+    uid: str
+    username: str
+    review: str | None = Field(default=None, max_length=REVIEW_MAX_LENGTH)
+    score: int = Field(..., gt=0, le=5)
+
+
 class TrainingPlan(BaseModel):
-    id: UUID = Field(default_factory=uuid4, alias="_id")
-    creator: UUID = Field(...) #User uuid
+    id: str = Field(default_factory=uuid4, alias="_id")
+    trainer: str = Field(...)  # User uuid
     title: str = Field(..., min_length=MIN_TITLE_LENGTH, max_length=MAX_TITLE_LENGTH)
     description: str | None = Field(default=None, max_length=MAX_DESCRIPTION_LENGTH)
-    difficulty: Difficulty 
+    difficulty: Difficulty
     training_types: list[str] = Field(...)
-    media: list[str] | None = Field(default=None, description="Multimedia resources (urls) associated with the training plan")
-    # Goals => ejercicios + tiempo + mas descanso
-    goals: list[str]
+    media: list[str] | None = Field(
+        default=None,
+        description="Multimedia resources (urls) associated with the training plan",
+    )
+    goals: list[str] = Field(...)
+    duration: int = Field(...)
+    reviews: list[Review] | None = Field(default=None)
+
     class Config:
         allow_population_by_field_name = True
         schema_extra = {
-            "example":
-            {
-            "id": "00010203-0405-0607-0809-0a0b0c0d0e0f",
-            "creator": "c59710ef-f5d0-41ba-a787-ad8eb739ef4c",
-            "title": "Sample training plan",
-            "description": "Training plan description",
-            "difficulty": "beginner",
-            "media": ["link-to-image", "link-to-video"],
-            "goals": ["plank: one minute"]
+            "example": {
+                "trainer": "c59710ef-f5d0-41ba-a787-ad8eb739ef4c",
+                "title": "Sample training plan",
+                "description": "Training plan description",
+                "difficulty": "beginner",
+                "training_types": ["cardio"],
+                "media": ["link-to-image", "link-to-video"],
+                "goals": ["plank: one minute"],
+                "duration": 90,
+                "reviews": None,
+            }
         }
-    }
-    
+
 
 class UpdateTrainingPlan(BaseModel):
-    title: str | None = Field(default=None,min_length=MIN_TITLE_LENGTH, max_length=MAX_TITLE_LENGTH) 
+    title: str | None = Field(
+        default=None, min_length=MIN_TITLE_LENGTH, max_length=MAX_TITLE_LENGTH
+    )
     description: str | None = Field(default=None, max_length=MAX_DESCRIPTION_LENGTH)
     difficulty: Difficulty | None
     training_types: list[str] | None = Field(default=None)
-    media: list[str] | None = Field(default=None, description="Multimedia resources (urls) associated with the training plan")
+    media: list[str] | None = Field(
+        default=None,
+        description="Multimedia resources (urls) associated with the training plan",
+    )
     goals: list[str] | None = None
+
+    class Config:
+        schema_extra = {
+            "example": {
+                "title": "Sample training plan",
+                "description": "Training plan description",
+                "difficulty": "beginner",
+                "media": ["link-to-image", "link-to-video"],
+                "goals": ["plank: one minute"],
+            }
+        }
--- app/api/trainers/routes.py
+++ app/api/trainers/routes.py
@@ -1,10 +1,9 @@
-from fastapi import APIRouter, Body, HTTPException, Request, status
+from fastapi import APIRouter, HTTPException, Request, status
 from fastapi.encoders import jsonable_encoder
 from fastapi.responses import JSONResponse
 from starlette.status import HTTP_404_NOT_FOUND
-from config.database import COLLECTION_NAME
+from app.config.database import COLLECTION_NAME
 from app.api.trainers.models import TrainingPlan, UpdateTrainingPlan
-from uuid import UUID
 
 router = APIRouter(tags=["plans"])
 
@@ -20,8 +19,8 @@ async def create_training_plan(plan: TrainingPlan, request: Request):
 
 
 # TODO: Does it make sense to make it idempotent
-@router.put("/plans/{id}")
-async def modify_training_plan(id: UUID, plan: UpdateTrainingPlan, request: Request):
+@router.put("/plans/{id}", response_model=TrainingPlan)
+async def modify_training_plan(id: str, plan: UpdateTrainingPlan, request: Request):
     updated_plan = {k: v for k, v in plan.dict().items() if v is not None}
 
     if len(updated_plan) > 0:
@@ -43,3 +42,14 @@ async def modify_training_plan(id: UUID, plan: UpdateTrainingPlan, request: Requ
     raise HTTPException(
         status_code=HTTP_404_NOT_FOUND, detail=f"Training plan {id} not found"
     )
+
+
+@router.get("/plans/{trainer_id}", response_model=list[TrainingPlan])
+async def get_training_plans(trainer_id: str, request: Request):
+    # May break if this is bigger than buffer
+    return [
+        plan
+        async for plan in request.app.mongodb[COLLECTION_NAME].find(
+            {"trainer": trainer_id}
+        )
+    ]
