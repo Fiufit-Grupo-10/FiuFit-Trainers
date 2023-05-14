f1cbf4fa70c234a7d286ae3da80f774e4b3301ba
Juan <jaguirreb@fi.uba.ar>
2023-05-14 20:59:18 -0300

    added cleanup for tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,17 +1,19 @@
 from pytest import fixture
 from starlette.config import environ
 from starlette.testclient import TestClient
-
+from app.main import app
+from app.config.database import DB_NAME
 @fixture
 def anyio_backend():
     return 'asyncio'
 
 @fixture(scope="session")
 def test_app():
-    from app.main import app
-
     with TestClient(app) as test_client:
         yield test_client
 
+@fixture(scope="session")
+def cleanup():
+    app.mongodb.drop_database(DB_NAME)
 
 environ["TESTING"] = "TRUE"
