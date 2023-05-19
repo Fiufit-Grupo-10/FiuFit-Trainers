e7af50123ab54d59d666c46a1e060ea727ad279a
Franco Gazzola <fgazzola@fi.uba.ar>
2023-05-19 08:53:54 -0300

    Add test for post review

--- tests/api/test_trainers.py
+++ tests/api/test_trainers.py
@@ -231,3 +231,47 @@ async def test_obtain_created_plans_of_certain_trainer():
 
     assert (json_result[0] == expected[0]) or (json_result[0] == expected[1])
     assert (json_result[1] == expected[0]) or (json_result[1] == expected[1])
+
+
+def test_create_review_without_errors(test_app):
+    review = {                
+                "plan_id": "c59710ef-f5d0-41ba-a787-ad8eb739ef4c",
+                "user_id": "7ca0fa95-af47-40b4-8e39-2fae5ee2667a",
+                "review": "Very good training",
+                "score": 2,
+    }
+    
+    response = test_app.post("/reviews", json=review)
+    assert response.status_code == 201
+
+    body = response.json()
+    assert "_id" in body
+
+def test_create_review_with_invalid_score(test_app):
+
+    review_1 = {                
+                "plan_id": "c59710ef-f5d0-41ba-a787-ad8eb739ef4c",
+                "user_id": "7ca0fa95-af47-40b4-8e39-2fae5ee2667a",
+                "review": "Very good training",
+                "score": 10,
+    }
+    
+    review_2 = {                
+                "plan_id": "c59710ef-f5d0-41ba-a787-ad8eb739ef4c",
+                "user_id": "7ca0fa95-af47-40b4-8e39-2fae5ee2667a",
+                "review": "Very good training",
+                "score": -2,
+    }
+
+    response = test_app.post("/reviews", json=review_1)
+    assert response.status_code == 422
+
+    response = test_app.post("/reviews", json=review_2)
+    assert response.status_code == 422
+
+
+
+
+
+
+
