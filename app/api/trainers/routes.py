77c75ca9a595d58acac40f26d6642782b0cb618b
Juan <jaguirreb@fi.uba.ar>
2023-06-05 13:45:23 -0300

    flake

--- app/api/trainers/routes.py
+++ app/api/trainers/routes.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, HTTPException, Request, Response, status, Query
+from fastapi import APIRouter, HTTPException, Request, status, Query
 from fastapi.encoders import jsonable_encoder
 from fastapi.responses import JSONResponse
 from starlette.status import HTTP_404_NOT_FOUND
